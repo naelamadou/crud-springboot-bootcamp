0c33ca5c9eb45628d011636e9914ee66d3448f73
naelamadou <naelamadou@gmail.com>
2024-07-17 20:16:15 +0000

    first commit

---
+++ src/main/java/com/cours/bootcamp/crud_springboot_bootcamp/CrudSpringbootBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.cours.bootcamp.crud_springboot_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cours/bootcamp/crud_springboot_bootcamp/CrudSpringbootBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cours.bootcamp.crud_springboot_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbootBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
